45e2f094aaef05980323006629e815098886f6c5
asdf1234Damian <asdf1234damian@gmail.com>
2019-02-16 23:23:14 -0600

    Cambios a la manera en la que las operaciones se manejan

--- View.py
+++ View.py
@@ -7,52 +7,19 @@ import Image
 automats= {}
 currAutomat = None
 
+# ---------------------------------------------------------Crea botones iguales
 class myButton:
-	def __init__( self, texto, frame ):
-		self.texto = texto
-		self.button = Button( frame, text = texto, fg = "white", bg = "#496ba0", activeforeground = "#f2f4f7", activebackground = "#354154", relief = 'flat', width = 25, height = 2, font = ( "Helvetica", 16 ) )
-		self.button.pack()
-	
-	def operacion( operacion, simbolo ):
-		def callback():
-			if operacion == "Opcional":
-				print(operacion)
-			if operacion == "Cerradura positiva":
-				print(operacion)
-			if operacion == "Cerradura de Kleene":
-				print(operacion)
-		t = threading.Thread(target=callback)
-		t.start()
-#Coloca el frame seleccionado hasta el frente
+    def __init__(self, texto, frame):
+        self.texto = texto
+        self.button = Button(frame, text=texto, fg="white", bg="#496ba0",
+        activeforeground="#f2f4f7", activebackground="#354154", relief='flat', width=25, height=2,
+        font=("Helvetica", 16))
+        self.button.pack()
+
+# Coloca el frame seleccionado hasta el frente
 def raise_frame(frame):
     frame.tkraise()
 
-#Al crear un nuevo autómata, recibe el símbolo del área de texto del formulario
-def valores(simbolo):
-	#Obtiene el aultómata seleccionado de la lista
-	value = [listbox.get(i) for i in listbox.curselection()]
-	#Verifica que se seleccionó un autómata
-	if value:
-		valor = value[0]
-		sim = simbolo.get()
-		labelM_F1.config( text = "  ")
-		#
-		if len(sim) > 0 :
-			f = AFND.Graph(valor, sim)
-			f.basico(sim)
-			f.plot()
-			labelM_F1.config( text = "  ")
-			filename = 'resources\Base.jpg'  
-		else:
-			labelM_F1.config( text = "Ingrese un símbolo válido")
-	else:
-		labelM_F1.config( text = "Seleccione un autómata")
-
-
-#------------------Crear ventana
-            filename = 'resources/' + valor + '.png'
-            filename = 'images/' + valor + '.png'
-            Image.ImageWidow(bottomFrame, path=filename)
 #Funcion para cambiar la imagen mostrada
 def cambiar_Imagen(id):
     global currAutomat
@@ -63,6 +30,54 @@ def cambiar_Imagen(id):
         currAutomat = id
     else:
         err_lbl_VerGrafo.config(text = 'No existe un autómata con esa ID')
+
+
+# Al crear un nuevo autómata, recibe el símbolo del área de texto del formulario
+def basico(id,simbolo):
+    if len(simbolo)>1:
+        err_lbl_CrearBasico.config(text = 'Ingrese un símbolo válido')
+    else:
+        if id in automats.keys():
+            err_lbl_CrearBasico.config(text = 'Ya existe un autómata')
+        else:
+            automats[id] = AFND.Graph(id,simbolo)
+            automats[id].basico(simbolo)
+            cambiar_Imagen(id)
+
+def Operaciones(operacion, f2 = None):
+    if f2:
+        if f2 in automats.keys():
+            if currAutomat == f2:
+                err_lbl_Operacion.config(text='Seleccione otro autómata')
+            else:
+                if operacion == 'Union':
+                    automats[currAutomat].unir(automats[f2])
+                    del automats[f2]
+                elif operacion == 'Concat':
+                    automats[currAutomat].concat(automats[f2])
+                    del automats[f2]
+                else:
+                    print('Error no existe operacion', operacion)
+                cambiar_Imagen(currAutomat)
+                err_lbl_Operacion.config(text = '')
+        else:
+            err_lbl_Operacion.config(text='No existe ese autómata')
+    else:
+        if currAutomat:
+            if operacion == 'Opcional':
+                automats[currAutomat].opcional()
+            elif operacion == 'CerrPos':
+                automats[currAutomat].cerradura_positiva()
+            elif operacion == 'CerrKle':
+                automats[currAutomat].cerradura_kleene()
+            else:
+                print('Error no existe operacion', operacion)
+            cambiar_Imagen(currAutomat)
+            err_lbl_Operacion.config(text = '')
+        else:
+            err_lbl_Operacion.config(text = 'No existe ese autómata')
+
+# ----------------------------------------------------------------Crear ventana
 root = Tk()
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 root.geometry("%dx%d+%d+0" % (w/2, h, w/2))
@@ -101,10 +116,6 @@ bttn_ElegirOperacion.button.config(command=lambda: raise_frame(menuOperacion))
 bttn_VerGrafo = myButton("Ver Otro Grafo", menuPrincipal)
 bttn_VerGrafo.button.config(command=lambda:raise_frame(menuVerGrafo))
 
-#------------------Menu---------------------------------
-menu.config(bg = "#496ba0")
-label_M = Label( menu, text = 'Elegir operacion', fg = 'white', bg ="#354154" , font = ( "Helvetica", 16 ) )
-label_M.pack( fill = 'x' )
 # ------------------------------------------------------------Menu Crear Basico
 menuCrearBasico.config(bg="#496ba0")
 lbl_CrearBasico = Label(menuCrearBasico, text='Crear autómata básico',
@@ -136,20 +147,47 @@ err_lbl_CrearBasico = Label(menuCrearBasico, text=" ", fg="red", bg="#496ba0",
 font=("Helvetica", 16))
 err_lbl_CrearBasico.pack()
 
-label_F1.pack( fill = 'x' )
+# -----------------------------------------------------------------------Menu Operaciones
+menuOperacion.config(bg="#496ba0")
+lbl_ElegirOperacionO = Label(menuOperacion, text='Elegir operacion', fg='white',
+                 bg="#354154", font=("Helvetica", 16))
+lbl_ElegirOperacionO.pack(fill='x')
+lbl_Op_Unarias = Label(menuOperacion, text='Operaciones Unarias', fg='white',
+                 bg="#354154", font=("Helvetica", 16))
+lbl_Op_Unarias.pack(fill='x')
 
-listbox = Listbox( f1, width = 25, height = 4, font = ( "Helvetica", 16 ) )
-listbox.pack()
+bttn_Opcional = myButton("Opcional", menuOperacion)
+bttn_Opcional.button.config(command=lambda:Operaciones('Opcional'))
+
+bttn_Cerr_Pos = myButton("Cerradura Positiva", menuOperacion)
+bttn_Cerr_Pos.button.config(command=lambda:Operaciones('CerrPos'))
+
+bttn_Cerr_Pos = myButton("Cerradura de Kleene", menuOperacion)
+bttn_Cerr_Pos.button.config(command=lambda:Operaciones('CerrKle'))
+
+lbl_Op_Binarias = Label(menuOperacion, text='Operaciones Binarias', fg='white',
+                 bg="#354154", font=("Helvetica", 16))
+lbl_Op_Binarias.pack(fill='x')
+
+lstbox_Binarias = Listbox(menuOperacion, width=25, height=4, font=("Helvetica", 16))
+lstbox_Binarias.pack()
 
 for item in ["F1", "F2", "F3", "F4"]:
-    listbox.insert(END, item)
+    lstbox_Binarias.insert(END, item)
+lstbox_Binarias.select_set(0)
 
+bttn_Union = myButton("Union", menuOperacion)
+bttn_Union.button.config(command=lambda:Operaciones('Union',lstbox_Binarias.get(ACTIVE)))
 
-buttonLB = myButton( "Seleccionar", f1)
-buttonLB.button.config( command = lambda:valores(simbol) )
+bttn_Concat = myButton("Concatenar", menuOperacion)
+bttn_Concat.button.config(command=lambda:Operaciones('Concat',lstbox_Binarias.get(ACTIVE)))
 
-buttonF1M = myButton( "Volver al menú", f1 )
-buttonF1M.button.config( command = lambda:raise_frame(menu) )
+bttn_back_Operacion = myButton("Volver al menú", menuOperacion)
+bttn_back_Operacion.button.config(command=lambda: raise_frame(menuPrincipal))
+
+err_lbl_Operacion = Label(menuOperacion, text=" ", fg="red", bg="#496ba0",
+font=("Helvetica", 16))
+err_lbl_Operacion.pack()
 # ---------------Ver Otro Grafo--------------------------
 menuVerGrafo.config(bg="#496ba0")
 label_VerGrafo = Label(menuVerGrafo, text='Elega un grafo', fg='white',
