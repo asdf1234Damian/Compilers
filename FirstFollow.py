74238d0edd0582a85a23648b98b67a97448f664f
Guillermo Naranjo <memoriasnaranjo98@gmail.com>
2019-03-13 14:57:58 -0600

    First y Follow

---
+++ FirstFollow.py
@@ -0,0 +1,13 @@
+class Gramatica:
+	def __init__(self):
+		self.reglas = {}
+		self.terminales = set()
+		self.noTerminales = set()
+		#Simbolo inicial de la gramatica
+		self.raiz = ""
+
+	def First(self, regla):
+		regla.split
+		self.reglas[]
+
+	def Follow():
