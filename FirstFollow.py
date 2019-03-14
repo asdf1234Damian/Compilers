cb70853d2613adb7c40f9cb8be0fd4e1667704f3
Damián Meza Madrid <asdf1234damian@gmail.com>
2019-03-13 18:14:40 -0600

    Funcion First  y constructor  de gramatica

--- FirstFollow.py
+++ FirstFollow.py
@@ -1,13 +1,63 @@
 class Gramatica:
-	def __init__(self):
+	def __init__(self, path):
 		self.reglas = {}
 		self.terminales = set()
 		self.noTerminales = set()
-		#Simbolo inicial de la gramatica
-		self.raiz = ""
+		self.raiz = None
+		#Crea el diccionario de reglas segun el archivo
+		with open(path, "r") as file:
+			file = file.readlines()	
+			# El estado inciial es el primer elemento en la primer linea
+			self.raiz = file[0].split()[0]
+			#Set con todos los simbolos en la gramatica, terminales y no terminales
+			simbolos = set()
+			# Se guardan todas las reglas en un mapa
+			for line in file:
+				line = line.split()
+				#Se borra la flecha de la tabla
+				del line[1]
+				simbolos.update(set(line))
+				izq = line[0]
+				self.noTerminales.add(izq)
+				der = line[1:]
+				if izq in self.reglas.keys():
+					self.reglas[izq].append(der)
+				else:
+					self.reglas[izq] = [der]
+			self.terminales = simbolos - self.noTerminales
+			# print('Raiz', self.raiz)
+			# print('Terminales',self.terminales)
+			# print('No Terminales', self.noTerminales)
+			# print('Reglas')
+			# for r in self.reglas.keys():
+			# 	for i in self.reglas[r]:
+			# 		print(r,'->' ,''.join(i))
 
-	def First(self, regla):
-		regla.split
-		self.reglas[]
+	def first(self, izq):
+		# Si es terminal, solo lo regresa
+		if izq in self.terminales:
+			return izq
+		else:
+			#Se guardan en orig todos los lados izq donde en la derecha se encuentra izq 
+			orig = []
+			for k in self.reglas.keys():
+				for der in self.reglas[k]:
+					if izq in der and izq not in orig:
+						orig.append(k)
+			#Se hace first recursivamente a todas las reglas guardadas en orig
+			res = []
+			for o in orig:
+				for der in self.reglas[o]:
+					f = self.first(der[0])
+					res.append(f)
+			# Se eliminan las repeticiones con set() y se regresa como lista 
+		return list(set(res))
 
-	def Follow():
+	
+			
+
+
+
+g  = Gramatica('testFiles/Gramatica.txt')
+print(g.first('E'))
+
--- README.md
+++ README.md
@@ -1,26 +1,7 @@
 # Compilers
-### &nbsp;&nbsp;&nbsp;&nbsp;Tarea 1:
+## Pemndientes:
 
-* Re hace GUI? [⏳]
-* Agregar boton para unir varios [⏳]
-* Agregar boton para generar tabla [⏳]
-* Arreglar cEpsilon recursion infinita [✔]
-* Subir la imagen de portada [✔]
-* Agregar funcione para unir todos o algunos grafos [✔]
-* Agregar funcione para validar una cadena [✔]
-* Hacer que el plot cree png en un tamaño determinado[✔]
-* Convertir AFN -> AFD [✔]
-* Imprimir en un txt la tabla del nuevo automata para optimizar despues [✔]
-* Crear funcion para convertir el archivo txt en una instancia del AFD [✔]
-* Implementar funcion que reciba n automatas y los una para analisis lexico [✔]
-* Hacer que borre el archivo dot[✔]
-* Analizar cadena &sigma; [✔]
-* Agregar GUI [✔]
-* Clases de automata [✔]
-* Agregar GraphViz [✔]
-* Crear automata básico [✔]
-* Unir automata F1 con F2 [✔]
-* Concatenar 2 AFN [✔]
-* Operacion Opcional [✔]
-* Cerradura+ [✔]
-* Cerradura* [✔]
+* Tabla de analisis lexico
+* First and Follow
+* Tabla LL1
+* Rehacer GUI con clases
